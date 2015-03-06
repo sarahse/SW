579139bb4c1f900b1a729dc1bd11340ee5113eb1
agunde email = and.gunde@gmail.com <and.gunde@gmail.com>
2015-03-06 13:13:07 +0100

    test

--- app/src/main/java/com/sarahserussi/mainactivity/GameLogic.java
+++ app/src/main/java/com/sarahserussi/mainactivity/GameLogic.java
@@ -16,6 +16,7 @@ public class GameLogic {
     private int speed2;
     private int speed3;
     private int speed4;
+    private int speed5;
 
     /* method that ensures the ball bounces off of the walls, but not the ceiling */
     private void bounceWall(){
