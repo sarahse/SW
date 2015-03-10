92602baf4d268b9d4a147d92c9a3418c01ba0e27
finnkj <finnkj92@gmail.com>
2015-03-10 13:36:07 +0100

    maintAct - fullscreen comment GameState

--- app/src/main/java/com/sarahserussi/mainactivity/GameState.java
+++ app/src/main/java/com/sarahserussi/mainactivity/GameState.java
@@ -1,9 +1,35 @@
 package com.sarahserussi.mainactivity;
 
+import android.view.MotionEvent;
+import android.view.WindowManager;
+
 /**
  * Created by sarahserussi on 04.03.15.
  */
 public class GameState {
 
     /* Where the touch methods go */
+
+    /* init */
+    public GameState () {
+        //add player
+        //set player to startPos
+        //add background
+        //add ball
+        //set ball to servePos
+        //set new score
+        
+    }
+
+    public void update (MotionEvent e) {
+        //move player
+        //if touchPos is left of playerPos - move player left
+        //if touchPos is right of playerPos - move player right
+        //player jumps
+    }
+
+    public boolean onTouchEvent (MotionEvent event){
+        //update(event);
+        return true;
+    }
 }
--- app/src/main/java/com/sarahserussi/mainactivity/MainActivity.java
+++ app/src/main/java/com/sarahserussi/mainactivity/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.WindowManager;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -11,6 +12,12 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
+        /* Set fullscreen */
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
     }
 
@@ -19,5 +26,7 @@ public class MainActivity extends ActionBarActivity {
         super.onDestroy();
 
     }
+
+
 
 }
